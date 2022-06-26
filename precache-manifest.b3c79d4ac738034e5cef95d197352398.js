10a8ab23a63d95ef7d6a63bc63fa89eb346dc7e9
zakaria-akash <zakaria.93@yahoo.com>
2022-06-27 05:46:14 +0600

    Updates

---
+++ precache-manifest.b3c79d4ac738034e5cef95d197352398.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9800d252857445519f9cb410e114865",
+    "url": "/e-StickyNotes/index.html"
+  },
+  {
+    "revision": "c3a6c60020cd9e04555e",
+    "url": "/e-StickyNotes/static/js/2.2c8a75cb.chunk.js"
+  },
+  {
+    "revision": "09797d709a6476b0f864",
+    "url": "/e-StickyNotes/static/js/main.3a87d134.chunk.js"
+  },
+  {
+    "revision": "2504d7975a9e668b70c7",
+    "url": "/e-StickyNotes/static/js/runtime-main.3172750c.js"
+  }
+]);
